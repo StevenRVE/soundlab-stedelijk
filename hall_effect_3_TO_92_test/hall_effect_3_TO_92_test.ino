69b25a1e2385f8c57e93fd6ae4b794a4a6e93098
Steven van Esch <steven_ve@live.nl>
2022-11-18 00:06:34 +0100

    minor changes

--- hall_effect_3_TO_92_test/hall_effect_3_TO_92_test.ino
+++ hall_effect_3_TO_92_test/hall_effect_3_TO_92_test.ino
@@ -1,14 +1,12 @@
-const unsigned int HALLEFFECT_INPUT_PIN_1 = A0;
-const unsigned int HALLEFFECT_INPUT_PIN_2 = A1;
-const unsigned int HALLEFFECT_INPUT_PIN_3 = A2;
-const unsigned int HALLEFFECT_INPUT_PIN_4 = A3;
-const unsigned int HALLEFFECT_INPUT_PIN_5 = A4;
-const unsigned int HALLEFFECT_INPUT_PIN_6 = A5;
-
-void printValues(unsigned int numOfSensor)
-{
-  
-}
+// Initialize pins
+const uint16_t HALLEFFECT_INPUT_PIN_1 = A0;
+const uint16_t HALLEFFECT_INPUT_PIN_2 = A1;
+const uint16_t HALLEFFECT_INPUT_PIN_3 = A2;
+const uint16_t HALLEFFECT_INPUT_PIN_4 = A3;
+const uint16_t HALLEFFECT_INPUT_PIN_5 = A4;
+const uint16_t HALLEFFECT_INPUT_PIN_6 = A5;
+const uint16_t HALLEFFECT_INPUT_PIN_7 = A6;
+const uint16_t HALLEFFECT_INPUT_PIN_8 = A7;
 
 void setup() {
   pinMode(HALLEFFECT_INPUT_PIN_1, INPUT);
@@ -23,27 +21,27 @@ void setup() {
 
 void loop() {
 
-  unsigned int hallEffectVal_1 = analogRead(HALLEFFECT_INPUT_PIN_1);
-  unsigned int hallEffectVal_2 = analogRead(HALLEFFECT_INPUT_PIN_2);
-  unsigned int hallEffectVal_3 = analogRead(HALLEFFECT_INPUT_PIN_3);
-  unsigned int hallEffectVal_4 = analogRead(HALLEFFECT_INPUT_PIN_4);
-  unsigned int hallEffectVal_5 = analogRead(HALLEFFECT_INPUT_PIN_5);
-  unsigned int hallEffectVal_6 = analogRead(HALLEFFECT_INPUT_PIN_6);
+  uint16_t hallEffectVal_1 = analogRead(HALLEFFECT_INPUT_PIN_1);
+  uint16_t hallEffectVal_2 = analogRead(HALLEFFECT_INPUT_PIN_2);
+  uint16_t hallEffectVal_3 = analogRead(HALLEFFECT_INPUT_PIN_3);
+  uint16_t hallEffectVal_4 = analogRead(HALLEFFECT_INPUT_PIN_4);
+  uint16_t hallEffectVal_5 = analogRead(HALLEFFECT_INPUT_PIN_5);
+  uint16_t hallEffectVal_6 = analogRead(HALLEFFECT_INPUT_PIN_6);
 
-  // Print the raw photocell value and the converted led value (e,g., for Serial 
-  // Console and Serial Plotter)
+  // Print the raw photocell value (e,g., for Serial Console and Serial Plotter)
   Serial.print("S1: ");
   Serial.print(hallEffectVal_1);
-  Serial.print(" || S2: ");
-  Serial.print(hallEffectVal_2);
-  Serial.print(" || S3: ");
-  Serial.print(hallEffectVal_3);
-  Serial.print(" || S4: ");
-  Serial.print(hallEffectVal_4);
-  Serial.print(" || S5: ");
-  Serial.print(hallEffectVal_5);
-  Serial.print(" || S6: ");
-  Serial.print(hallEffectVal_6);
+  //  Serial.print(" || S2: ");
+  //  Serial.print(hallEffectVal_2);
+  //  Serial.print(" || S3: ");
+  //  Serial.print(hallEffectVal_3);
+  //  Serial.print(" || S4: ");
+  //  Serial.print(hallEffectVal_4);
+  //  Serial.print(" || S5: ");
+  //  Serial.print(hallEffectVal_5);
+  //  Serial.print(" || S6: ");
+  //  Serial.print(hallEffectVal_6);
+  
   Serial.println();
 
   delay(20); // read at 50Hz
