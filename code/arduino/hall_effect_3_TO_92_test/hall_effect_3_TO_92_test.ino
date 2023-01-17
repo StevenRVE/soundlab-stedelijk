b39058c5eb47b9b09c4a6a6b37f2fa1ace44066b
Steven van Esch <steven_ve@live.nl>
2023-01-17 13:19:52 +0100

    added arduino code for presentation demo

--- code/arduino/hall_effect_3_TO_92_test/hall_effect_3_TO_92_test.ino
+++ code/arduino/hall_effect_3_TO_92_test/hall_effect_3_TO_92_test.ino
@@ -1,20 +1,23 @@
 // Initialize pins
 const uint16_t HALLEFFECT_INPUT_PIN_1 = A0;
-const uint16_t HALLEFFECT_INPUT_PIN_2 = A1;
-const uint16_t HALLEFFECT_INPUT_PIN_3 = A2;
-const uint16_t HALLEFFECT_INPUT_PIN_4 = A3;
-const uint16_t HALLEFFECT_INPUT_PIN_5 = A4;
-const uint16_t HALLEFFECT_INPUT_PIN_6 = A5;
-const uint16_t HALLEFFECT_INPUT_PIN_7 = A6;
-const uint16_t HALLEFFECT_INPUT_PIN_8 = A7;
+
+// Define the number of samples to keep track of. The higher the number, the
+// more the readings will be smoothed, but the slower the output will respond to
+// the input. Using a constant rather than a normal variable lets us use this
+// value to determine the size of the readings array.
+const int numReadings = 10;
+
+int readings[numReadings];  // the readings from the analog input
+int readIndex = 0;          // the index of the current reading
+int total = 0;              // the running total
+int average = 0;            // the average
 
 void setup() {
   pinMode(HALLEFFECT_INPUT_PIN_1, INPUT);
-  pinMode(HALLEFFECT_INPUT_PIN_2, INPUT);
-  pinMode(HALLEFFECT_INPUT_PIN_3, INPUT);
-  pinMode(HALLEFFECT_INPUT_PIN_4, INPUT);
-  pinMode(HALLEFFECT_INPUT_PIN_5, INPUT);
-  pinMode(HALLEFFECT_INPUT_PIN_6, INPUT);
+
+  for (int thisReading = 0; thisReading < numReadings; thisReading++){
+    readings[thisReading] = 0;
+  }
   
   Serial.begin(9600);
 }
@@ -22,26 +25,31 @@ void setup() {
 void loop() {
 
   uint16_t hallEffectVal_1 = analogRead(HALLEFFECT_INPUT_PIN_1);
-  uint16_t hallEffectVal_2 = analogRead(HALLEFFECT_INPUT_PIN_2);
-  uint16_t hallEffectVal_3 = analogRead(HALLEFFECT_INPUT_PIN_3);
-  uint16_t hallEffectVal_4 = analogRead(HALLEFFECT_INPUT_PIN_4);
-  uint16_t hallEffectVal_5 = analogRead(HALLEFFECT_INPUT_PIN_5);
-  uint16_t hallEffectVal_6 = analogRead(HALLEFFECT_INPUT_PIN_6);
+
+  // subtract the last reading:
+  total = total - readings[readIndex];
+  // read from the sensor:
+  readings[readIndex] = hallEffectVal_1;
+  // add the reading to the total:
+  total = total + readings[readIndex];
+  // advance to the next position in the array:
+  readIndex = readIndex + 1;
+
+  // if we're at the end of the array...
+  if (readIndex >= numReadings) {
+    // ...wrap around to the beginning:
+    readIndex = 0;
+  }
+
+  // calculate the average:
+  average = total / numReadings;
+  // send it to the computer as ASCII digits
+  Serial.print(average);
+  delay(1);  // delay in between reads for stability
 
   // Print the raw photocell value (e,g., for Serial Console and Serial Plotter)
-  Serial.print("S1: ");
-  Serial.print(hallEffectVal_1);
-  //  Serial.print(" || S2: ");
-  //  Serial.print(hallEffectVal_2);
-  //  Serial.print(" || S3: ");
-  //  Serial.print(hallEffectVal_3);
-  //  Serial.print(" || S4: ");
-  //  Serial.print(hallEffectVal_4);
-  //  Serial.print(" || S5: ");
-  //  Serial.print(hallEffectVal_5);
-  //  Serial.print(" || S6: ");
-  //  Serial.print(hallEffectVal_6);
-  
+  // Serial.print(hallEffectVal_1);
+
   Serial.println();
 
   delay(20); // read at 50Hz
