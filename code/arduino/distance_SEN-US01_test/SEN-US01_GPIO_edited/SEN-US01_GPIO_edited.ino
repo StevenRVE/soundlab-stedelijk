ed56322a8a83d7c41c9aa901a8708968fbd4e12b
Steven van Esch <steven_ve@live.nl>
2023-01-25 17:54:28 +0100

    Updated SEN-US01_GPIO_edited to handle 10 sensors

--- code/arduino/distance_SEN-US01_test/SEN-US01_GPIO_edited/SEN-US01_GPIO_edited.ino
+++ code/arduino/distance_SEN-US01_test/SEN-US01_GPIO_edited/SEN-US01_GPIO_edited.ino
@@ -1,22 +1,49 @@
 float       distance, duration;
-const int   echo1=A0; //selected pins
-const int   trigger1=A1;
-const int   echo2=A2;
-const int   trigger2=A3;
-
+const int   echo1=2, echo2=3, echo3=4, echo4=5, echo5=6, echo6=7, echo7=8, echo8=9, echo9=10, echo10=11;
+const int   trigger1=12, trigger2=13, trigger3=14, trigger4=15, trigger5=16, trigger6=17, trigger7=18, trigger8=19, trigger9=20, trigger10=21;
 
 void setup(){
     Serial.begin(9600);
     pinMode(echo1,INPUT);
-    pinMode(trigger1,OUTPUT);
     pinMode(echo2,INPUT);
-    pinMode(trigger2,OUTPUT);    
+    pinMode(echo3,INPUT);
+    pinMode(echo4,INPUT);
+    pinMode(echo5,INPUT);
+    pinMode(echo6,INPUT);
+    pinMode(echo7,INPUT);
+    pinMode(echo8,INPUT);
+    pinMode(echo9,INPUT);
+    pinMode(echo10,INPUT);
+    pinMode(trigger1,OUTPUT);
+    pinMode(trigger2,OUTPUT);
+    pinMode(trigger3,OUTPUT);
+    pinMode(trigger4,OUTPUT);
+    pinMode(trigger5,OUTPUT);
+    pinMode(trigger6,OUTPUT);
+    pinMode(trigger7,OUTPUT);
+    pinMode(trigger8,OUTPUT);
+    pinMode(trigger9,OUTPUT);
+    pinMode(trigger10,OUTPUT);    
 }
 
 void loop(){
 
+    // Serial.print(20);
+    // Serial.print(",");
+    // Serial.print(50);
+    // Serial.print(",");
     readSensor(echo1, trigger1, 1);
     readSensor(echo2, trigger2, 2);
+    readSensor(echo3, trigger3, 3);
+    readSensor(echo4, trigger4, 4);
+    // readSensor(echo5, trigger5, 5);
+    // readSensor(echo6, trigger6, 6);
+    // readSensor(echo7, trigger7, 7);
+    // readSensor(echo8, trigger8, 8);
+    // readSensor(echo9, trigger9, 9);
+    // readSensor(echo10, trigger10, 10);
+
+    Serial.println();
 
     delay(20);    // Pause between the individual measurements
 }
@@ -29,9 +56,13 @@ void readSensor(int echoPin, int triggerPin, int sensorNumber)
     duration  = pulseIn(echoPin,HIGH);
     distance  = duration*340/2/10000;
     if (distance < 2 || distance > 450){        // Checking whether measured value is within the permissible distance
-        Serial.println("distance is outside of the measuring range");   // If not, an error message is output
+        // Serial.println("distance is outside of the measuring range");   // If not, an error message is output
+        Serial.print(120);
+        Serial.print(",");
     }
     else{
-        Serial.println("distance sensor " + String(sensorNumber) + ": " + String(distance) + " cm");    // The calculated distance is output to the console
+        // Serial.println("distance sensor " + String(sensorNumber) + ": " + String(distance) + " cm");    // The calculated distance is output to the console
+        Serial.print(distance);
+        Serial.print(",");
     }
 }
